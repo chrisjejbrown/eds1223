fef09837736d9d1618d3aed07ba137eb9b271f38
Chris Brown <chrbrown@adobe.com>
2024-02-07 09:00:59 -0500

    qqwe

--- blocks/cbheader/cbheader.js
+++ blocks/cbheader/cbheader.js
@@ -1,5 +1,14 @@
 export default function decorate(block) {
-  console.log(block);
+  var result = [];
+  const regex = />([a-zA-Z \d\!]+)</gm;
+  const str = block.querySelectorAll('body *:not(style,script)')[0].innerHTML;
+  let m;
+  
+  while ((m = regex.exec(str)) !== null) {
+    result.push(m[1]);
+  }
+  
+  console.log(result);
 
   }
     
