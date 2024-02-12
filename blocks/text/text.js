7dd8c770434fb99ae6b45fc4f28c8f220c07163e
Chris Brown <chrbrown@adobe.com>
2024-02-12 09:28:30 -0500

    qefd

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -1,22 +1,22 @@
 export default function decorate(block) {
-  var result = [];
+/*  var result = [];
   const regex = />([a-zA-Z \d\!]+)</gm;
   const str = block.querySelectorAll('body *:not(style,script)')[0].innerHTML;
   let m;
   
   while ((m = regex.exec(str)) !== null) {
     result.push(m[1]);
-  }
+  }*/
   
  /* console.log(result);*/
 
-  delete block.outerHTML;
+  /*delete block.outerHTML;
   var myHTMLstart = "<h1>";
   var myHTMLend = "</h1>";
 
   var video = '<video src="https://reebok.bynder.com/m/4f85f9f282e0f0ae/original/24SS_Create-What-Makes-You_HP-Masthead_DT.mp4" preload="auto" autoplay="" loop="" playsinline="" webkit-playsinline="" x5-playsinline="" style="width: 100%; height: 100%;"></video>';
 
   block.outerHTML = video;
-  console.log(block.outerHTML);
+  console.log(block.outerHTML);*/
   }
     
